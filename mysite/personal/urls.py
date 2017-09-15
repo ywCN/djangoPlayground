9f52e06ad838f8bb132a7d6e6e2935542cc380c6
ywCN <wangyouhao90@gmail.com>
2017-09-15 18:33:05 -0400

    try to add contact info

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import url, include
 from . import views  # import from local packages
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index')]
+    url(r'^$', views.index, name = 'index'),
+    url(r'^contact/', views.contact, name = 'contact'),  # ^contact/$ works too
+    
+    ]
