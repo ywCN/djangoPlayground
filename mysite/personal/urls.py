ea716ac023ff4a3fff51a2e359ea531a32a8cb55
ywCN <wangyouhao90@gmail.com>
2017-09-15 10:32:01 -0400

    debugging

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
 from . import views  # import from local packages
+
 urlpatterns = [
-    url(r'^$/', views.index, name = 'index'),
-]
+    url(r'^$', views.index, name = 'index')]
