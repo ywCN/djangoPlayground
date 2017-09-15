621a8e252c2bf1ef821123c7f70c8dc693467c75
ywCN <wangyouhao90@gmail.com>
2017-09-15 19:49:30 -0400

    building more blog functionality

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-class Post(models.Model):
+class Post(models.Model):  # class is the table, each field is col in table
     title = models.CharField(max_length=140)
     body = models.TextField()
     date = models.DateTimeField()
 
     def __str__(self):
         return self.title
-    
