60561be373993bda932f8a17f6a2b930c8498143
ywCN <wangyouhao90@gmail.com>
2017-09-15 18:43:40 -0400

    debugged

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -3,6 +3,6 @@ from . import views  # import from local packages
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^contact/', views.contact, name = 'contact'),  # ^contact/$ works too
+    url(r'^contact/', views.contact, name = 'contact'),  # ^contact/$ works too, and use $ is better because only correct typed url will go to /contact page
     
     ]
