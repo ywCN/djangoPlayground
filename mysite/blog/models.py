05e9a61d317c36a972c66b7a97429c243e43ed33
ywCN <wangyouhao90@gmail.com>
2017-09-15 20:34:52 -0400

    fixed suspicious bug

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 class Post(models.Model):  # class is the table, each field is col in table
+
     title = models.CharField(max_length=140)
     body = models.TextField()
     date = models.DateTimeField()
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-urlpatterns = [ url(r'^blog/', ListView.as_view(queryset=Post.object.all().order_by("-date")[:25],
-template_name="blog/blog.html"))]
+urlpatterns = [ 
+    url(r'^blog/$', 
+    ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],
+    template_name="blog/blog.html")),
+    ]
